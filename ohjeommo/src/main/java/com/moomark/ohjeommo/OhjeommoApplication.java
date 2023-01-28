2a794e8b047940bea6651ffdffd65c6890cf0e57
Apisapple <38449976+Apisapple@users.noreply.github.com>
2023-01-29 04:01:19 +0900

    Init project
    Init spring project

---
+++ ohjeommo/src/main/java/com/moomark/ohjeommo/OhjeommoApplication.java
@@ -0,0 +1,13 @@
+package com.moomark.ohjeommo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhjeommoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OhjeommoApplication.class, args);
+  }
+
+}
